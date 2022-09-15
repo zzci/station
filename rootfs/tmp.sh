6e69481771b7db520d25d6405755c0e930b8d7f0
Roy <62500486+aazf@users.noreply.github.com>
2022-09-15 22:46:44 +0700

    Update tmp.sh

--- rootfs/tmp.sh
+++ rootfs/tmp.sh
@@ -4,15 +4,17 @@ _p_w_d=$(pwd)
 > /etc/motd
 
 sed -i '1,/robbyrussell/{s/robbyrussell/afowler/}' ~/.zshrc
+
 echo 'source ~/.alias' >> ~/.zshrc
+
 echo "zstyle ':omz:update' mode disabled" >> ~/.zshrc
 
-echo 'PATH=$PATH:/work/bin:/build/bin' >> ~/.zshrc
-echo 'PATH=$PATH:/work/bin:/build/bin' >> ~/.bashrc
 usermod --shell /bin/zsh root
 
 ## fix github action file permissions
+
 chmod -R 0755 /build /root
+
 chmod -R 0644 /build/config /build/services /root/.alias
 
 cd $_p_w_d && rm -f $0
